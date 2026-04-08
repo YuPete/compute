c4c12afe9bdac46ab994d08570b76b5c2b1f5b5b
Peter Yu <hy2846@columbia.edu>
2026-04-08 15:33:26 -0400

    defined is_cpu_allowed

--- linux/kernel/sched/heater.c
+++ linux/kernel/sched/heater.c
@@ -1,7 +1,41 @@
 #include "sched.h"
+#include <linux/mmu_context.h>
 
 LIST_HEAD(global_rq);
 DEFINE_RAW_SPINLOCK(global_rq_lock);
+
+
+/*
+ * Per-CPU kthreads are allowed to run on !active && online CPUs, see
+ * __set_cpus_allowed_ptr() and select_fallback_rq().
+ */
+static inline bool is_cpu_allowed(struct task_struct *p, int cpu)
+{
+	/* When not in the task's cpumask, no point in looking further. */
+	if (!cpumask_test_cpu(cpu, p->cpus_ptr))
+		return false;
+
+	/* migrate_disabled() must be allowed to finish. */
+	if (is_migration_disabled(p))
+		return cpu_online(cpu);
+
+	/* Non kernel threads are not allowed during either online or offline. */
+	if (!(p->flags & PF_KTHREAD))
+		return cpu_active(cpu) && task_cpu_possible(cpu, p);
+
+	/* KTHREAD_IS_PER_CPU is always allowed. */
+	if (kthread_is_per_cpu(p))
+		return cpu_online(cpu);
+
+	/* Regular kernel threads don't get to stay during offline. */
+	if (cpu_dying(cpu))
+		return false;
+
+	/* But are allowed during online. */
+	return cpu_online(cpu);
+}
+
+
 
 void init_heater_rq(struct heater_rq *heater_rq)
 {
