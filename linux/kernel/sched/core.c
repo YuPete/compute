2f0fef0522f21114b44b0f37acc7ea7c0372d9db
Cynthia Zhang <2012cynthiazhang@gmail.com>
2026-04-08 18:07:21 -0400

    prio init task and kthreads and ordering

--- linux/kernel/sched/core.c
+++ linux/kernel/sched/core.c
@@ -9925,9 +9925,9 @@ void __init sched_init(void)
 	/* Make sure the linker didn't screw up */
 
 	BUG_ON(&idle_sched_class != &heater_sched_class + 1 ||
-		&heater_sched_class != &freezer_sched_class + 1 ||
-		&freezer_sched_class != &fair_sched_class + 1 ||
-	       &fair_sched_class != &rt_sched_class + 1 ||
+		&heater_sched_class != &fair_sched_class + 1 ||
+		&fair_sched_class != &freezer_sched_class + 1 ||
+	       &freezer_sched_class != &rt_sched_class + 1 ||
 	       &rt_sched_class   != &dl_sched_class + 1);
 
 #ifdef CONFIG_SMP
